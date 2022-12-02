ba948f419d4d455b40119d2a970db8e84f21c138
elyx17 <alexdiazvera97@gmail.com>
2022-12-02 11:33:13 -0300

    carrito

--- js/main.js
+++ js/main.js
@@ -1,53 +1,49 @@
-const producto = document.querySelector("div#negroni")
 
-//Guardar y Recuperar el Carrito con LocalStorage + JSON
+const tbody = document.querySelector('tbody')
+
 const carrito = []
-const guardarCarrito = ()=> (carrito.length > 0) && localStorage.setItem("CarritoTragos", JSON.stringify(carrito))
-const recuperarCarrito = ()=> JSON.parse(localStorage.getItem("CarritoTragos")) || []
+const guardarCarrito = ()=> (carrito.length > 0) && localStorage.setItem("carritoTragos", JSON.stringify(carrito))
+const recuperarCarrito = ()=> JSON.parse(localStorage.getItem("carritoTragos")) || []
 carrito.push(...recuperarCarrito())
 
-//Armar la tabla HTML dinámica
-//{imagen: '🩱', codigo: 9, tipo: 'Malla enteriza Lafelí', precio: 3122}
-const armarTablaHTML = (trago)=> {
-    return `<div class="card" id="negroni" >
-            <div class="card__textos">
-            <h3>${trago.tipo}</h3>
-            <h3>$ ${trago.precio}</h3>
-            <p>
-                <button id="${trago.codigo} class="button button-outline" title="Agregar al carrito">Comprar</button>
-            </p>
-            </div>
-        </div>`
+const armarTablaHTML = (trago) => {
+    return `                
+    <tr>
+        <td><h2>${trago.imagen}</h2></td>
+        <td>${trago.name}</td>
+        <td>$ ${trago.precio}</td>
+        <td>
+            <button id="${trago.codigo}" class="buttonTable">Comprar</button>
+        </td>
+    </tr>
+    `
 }
-
-//Cargar los productos en la tabla HTML
 
 const cargarProductos = (array)=> {
     let tablaHTML = ""
         if (array.length > 0) {
-            array.forEach((trago) => tablaHTML += armarTablaHTML(trago))
+            array.forEach((prenda) => tablaHTML += armarTablaHTML(prenda))
         } else {
             tablaHTML = "<h2 class='error-prendas'>Error al cargar productos.</h2>"
         }
-        tbody.innerHTML = tablaHTML
-}
+        tbody.innerHTML = tablaHTML }
 
-//Activar el evento CLICK por cada botón dinámico generado
 const activarClickBotonesAdd = ()=> {
-    const botonesAdd = document.querySelectorAll("button.button.button-outline")
-          botonesAdd.forEach(btn => {
+    const botonesAdd = document.querySelectorAll("button.buttonTable")
+            botonesAdd.forEach(btn => {
             btn.addEventListener("click", (e)=> {
-                let resultado = buscarPrenda(e.target.id)
+                let resultado = buscarTrago(e.target.id)
                     carrito.push(resultado)
                     guardarCarrito()
             })
-          })
+        })
 }
 
-cargarProductos(prendas)
+
+cargarProductos(tragos)
 activarClickBotonesAdd()
 
-const buscarPrenda = (codigo)=> prendas.find(trago => trago.codigo === parseInt(codigo))
+const buscarTrago = (codigo)=> tragos.find(trago => trago.codigo === parseInt(codigo))
 
 function comprar() {
     let codigo = prompt(mensajeInicial)
---
+++ js/productos.js
@@ -0,0 +1,7 @@
+const tragos = [{imagen: '🥃', codigo: 1, name: 'Negroni', precio: 1200},
+                 {imagen: '🍹', codigo: 2, name: 'Mojito', precio: 1100},
+                 {imagen: '🍷', codigo: 3, name: 'Bramble', precio: 1000},
+                 {imagen: '☕', codigo: 4, name: 'Espresso Martini', precio: 1300},
+                 {imagen: '🍺', codigo: 5, name: 'Moscow Mule', precio: 1500},
+                 {imagen: '🍸', codigo: 6, name: 'NewYork Sour', precio: 1400},
+]
